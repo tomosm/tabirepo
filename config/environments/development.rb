32cbca9d304276ca3ac2d3fa8725dea8f7326143
tomonori_murakami <murakami@eviry.com>
2013-04-02 02:51:03 +0900

    change host from localhost to tabirepo.tk

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,8 @@ TabiRepo::Application.configure do
   # 1. Ensure you have defined default url options in your environments files. Here
   #    is an example of default_url_options appropriate for a development environment
   #    in config/environments/development.rb:
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'tabirepo.tk' }
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
